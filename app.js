a92a637b07986dc928c0dfde43fd223fcc63bdea
Alex Grachev <alexvirus@mail.ru>
2019-04-01 21:08:38 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const Koa = require('koa');
+const app = new Koa();
+
+const Router = require('koa-router');
+const router = new Router();
+
+router.get('/',async (ctx) => {
+    console.log('----------------');
+    console.log(ctx.request);
+    console.log('----------------');
+    console.log(ctx.response);
+    console.log('----------------');
+    console.log(ctx.app);
+    ctx.body = 'Hello World';
+});
+
+app.use(router.routes());
+
+app.listen(3000);
